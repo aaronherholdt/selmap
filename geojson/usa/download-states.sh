ba49feadf86032264c535b75b8aeec620ed3e2b1
aaronherholdt <your-email@example.com>
2025-02-13 01:25:41 +0200

    Updates

---
+++ geojson/usa/download-states.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+states=(
+  "alabama" "alaska" "arizona" "arkansas" "california" "colorado" "connecticut" 
+  "delaware" "florida" "georgia" "idaho" "illinois" "indiana" "iowa" 
+  "kansas" "kentucky" "louisiana" "maine" "maryland" "massachusetts" "michigan" 
+  "minnesota" "mississippi" "missouri" "montana" "nebraska" "nevada" 
+  "new_hampshire" "new_jersey" "new_mexico" "new_york" "north_carolina" 
+  "north_dakota" "ohio" "oklahoma" "oregon" "pennsylvania" "rhode_island" 
+  "south_carolina" "south_dakota" "tennessee" "texas" "utah" "vermont" 
+  "virginia" "washington" "west_virginia" "wisconsin" "wyoming"
+)
+
+for state in "${states[@]}"; do
+  curl -o "${state}.json" "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states/${state}.json"
+done 
